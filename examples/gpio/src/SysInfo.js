8ac77ba8113d8e2a65ddde064f52dda894f2cb4d
fernando.lima <fol2111@gmail.com>
2018-09-06 12:43:57 -0300

    Added more informations

--- examples/gpio/src/SysInfo.js
+++ examples/gpio/src/SysInfo.js
@@ -1,24 +1,71 @@
 const {execSync} = require('child_process');
 const si = require('systeminformation');
 
+/**
+ * 
+ * 
+ * @returns 
+ */
 function cpuTemperatureSync() {
     try {
         let tempInfo = {};
-        let result  = [];
+        let result = [];
         execSync('cat /sys/class/thermal/thermal_zone*/temp')
             .toString()
             .split('\n')
             .forEach(element => {
-               if(parseInt(element)) {
-                   result.push(parseInt(element)/1000);
-               }
+                if (parseInt(element)) {
+                    result.push(parseInt(element) / 1000);
+                }
+                else
+                    result.push(-1);
+
             });
-            tempInfo.max = result[0];
-            tempInfo.main = result[1];
+        tempInfo.max = result[0];
+        tempInfo.main = result[1];
         return tempInfo;
 
     } catch (error) {
         return null;
     }
 }
+/**
+ * 
+ * 
+ * @returns Promise
+ */
+async function cpuTemperature()
+{
+    try {
+        return await si.cpuTemperature();
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
 
+async function systemInformations() {
+    try {
+        let bundle = {
+            cpu: {},
+            currentSpeed: {},
+            temperature: {},
+            memory: {}
+        };
+        bundle.cpu = await si.cpu();
+        bundle.currentSpeed = await si.cpuCurrentspeed();
+        bundle.temperature = await si.cpuTemperature();
+        bundle.memory = await si.mem();
+        return bundle;
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+module.exports = {
+    cpuTemperature,
+    cpuTemperatureSync,
+    systemInformations
+};
